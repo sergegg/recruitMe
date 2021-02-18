5ea0bf621f2a75fa39ea9b39798776ae679fee26
sergey honiyan <sergehoniyan@gmail.com>
2021-02-18 14:21:56 -0500

    small change to commit to heroku

--- api/views/user_views.py
+++ api/views/user_views.py
@@ -68,7 +68,7 @@ class SignIn(generics.CreateAPIView):
             else:
                 return Response({ 'msg': 'The account is inactive.' }, status=status.HTTP_400_BAD_REQUEST)
         else:
-            return Response({ 'msg': 'The username and/or password is incorrect.' }, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+            return Response({ 'msg': 'The username and/or Password is incorrect.' }, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
 
 class SignOut(generics.DestroyAPIView):
     def delete(self, request):
